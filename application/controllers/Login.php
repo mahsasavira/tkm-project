c4ebcded1b7ada01596bf9b4187f07c92f3fa1f6
MonicaTifaniZ <tyzahra99@gmail.com>
2021-09-17 20:47:49 +0700

    test

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,6 +7,7 @@ class Auth extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->helper('url');
     }
 
 
--- application/views/transaksi/neraca.php
+++ application/views/transaksi/neraca.php
@@ -130,7 +130,7 @@
                                 </div>
                             </div>
                             <div class="col-md-0 mt-4">
-                                <div class="col-md-4">
+                                <div class="col-md-8">
                                     <div class="card">
                                         <div class="card-body pt-4 p-2">
                                             <ul class="list-group">
@@ -228,7 +228,7 @@
                                 </div>
                             </div>
                             <div class="col-md-0 mt-4">
-                                <div class="col-md-4">
+                                <div class="col-md-8">
                                     <div class="card">
                                         <div class="card-body pt-4 p-2">
                                             <ul class="list-group">
@@ -690,7 +690,7 @@
         </div>
     </main>
 
-    <!-- <div class="fixed-plugin">
+    <div class="fixed-plugin">
         <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
             <i class="fa fa-cog py-2"> </i>
         </a>
@@ -705,12 +705,12 @@
                         <i class="fa fa-close"></i>
                     </button>
                 </div> -->
-    <!-- End Toggle Button -->
-    <!-- </div>
+                <!-- End Toggle Button -->
+            </div>
             <hr class="horizontal dark my-1">
             <div class="card-body pt-sm-3 pt-0"> -->
-    <!-- Sidebar Backgrounds -->
-    <!-- <div>
+                <!-- Sidebar Backgrounds -->
+                <div>
                     <h6 class="mb-0">Sidebar Colors</h6>
                 </div>
                 <a href="javascript:void(0)" class="switch-trigger background-color">
@@ -723,8 +723,8 @@
                         <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
                     </div>
                 </a> -->
-    <!-- Sidenav Type -->
-    <!-- <div class="mt-3">
+                <!-- Sidenav Type -->
+                <div class="mt-3">
                     <h6 class="mb-0">Sidenav Type</h6>
                     <p class="text-sm">Choose between 2 different sidenav types.</p>
                 </div>
@@ -732,31 +732,10 @@
                     <button class="btn bg-gradient-primary w-100 px-3 mb-2 active" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
                     <button class="btn bg-gradient-primary w-100 px-3 mb-2 ms-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
                 </div>
-                <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p> -->
-    <!-- Navbar Fixed -->
-    <!-- <div class="mt-3">
-                    <h6 class="mb-0">Navbar Fixed</h6>
-                </div>
-                <div class="form-check form-switch ps-0">
-                    <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
-                </div>
-                <hr class="horizontal dark my-sm-4">
-                <a class="btn bg-gradient-dark w-100" href="https://www.creative-tim.com/product/soft-ui-dashboard-pro">Free
-                    Download</a>
-                <a class="btn btn-outline-dark w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/license/soft-ui-dashboard">View documentation</a>
-                <div class="w-100 text-center">
-                    <a class="github-button" href="https://github.com/creativetimofficial/soft-ui-dashboard" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/soft-ui-dashboard on GitHub">Star</a>
-                    <h6 class="mt-3">Thank you for sharing!</h6>
-                    <a href="https://twitter.com/intent/tweet?text=Check%20Soft%20UI%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fsoft-ui-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-                        <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
-                    </a>
-                    <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/soft-ui-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-                        <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
-                    </a>
-                </div>
+                <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
             </div>
         </div>
-    </div> -->
+    </div>
     <!--   Core JS Files   -->
     <script src="<?php echo base_url() ?>assets/js/core/popper.min.js"></script>
     <script src="<?php echo base_url() ?>assets/js/core/bootstrap.min.js"></script>
