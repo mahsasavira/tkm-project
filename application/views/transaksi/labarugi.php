9bd3313c2040b379865239bfe64d241c67d97987
Mahsa Savira <mahsasafira2@gmail.com>
2021-09-18 23:00:04 +0700

    Update labarugi.php

--- application/views/transaksi/labarugi.php
+++ application/views/transaksi/labarugi.php
@@ -27,8 +27,6 @@
                                 <span class="d-sm-inline d-none">Keluar</span>
                             </a>
                         </li>
-
-
                         <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
                             <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
                                 <div class="sidenav-toggler-inner">
