153a7c0ff501e429c8708e243796d0a11da3d1d7
Mahsa Savira <mahsasafira2@gmail.com>
2021-09-28 23:51:26 +0700

    Update edit_aktiva.php

--- application/views/transaksi/edit_aktiva.php
+++ application/views/transaksi/edit_aktiva.php
@@ -5,7 +5,7 @@
             <div class="container-fluid py-1 px-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="<?= 'neraca' ?>">Neraca</a></li>
+                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="#">Neraca</a></li>?
                         <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Edit Aktiva</li>
                     </ol>
                     <h6 class="font-weight-bolder mb-0">Edit Aktiva</h6>
@@ -79,6 +79,7 @@
                                             </div>
                                         </ul>
                                     </div>
+                                    <!-- <<<<<<< Updated upstream
                                     <div class="card-body p-3">
                                         <div class="table-responsive p-0">
                                             <?php foreach ($neraca as $ner) { ?>
@@ -179,43 +180,143 @@
                                                         <div class="form-group col-5">
                                                             <label>Akum.Peny.-Kamera Video</label>
                                                             <input type="number" name="akum_peny_video" value="<?php echo $ner->akum_peny_video ?>" class="form-control">
+======= -->
+                                    <form action="" method="post">
+                                        <div class="card-body p-3">
+                                            <div class="table-responsive p-0">
+                                                <div class="col-12 px-3">
+                                                    <div class="row">
+                                                        <div class="form-group col-5">
+                                                            <label>Kas Kecil</label>
+                                                            <input type="number" name="kas_kecil" value="<?= $this->input->post('kas_kecil') ?? $queryeditakt->KAS_KECIL ?>" class="form-control" required>
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Kas Pada Bank</label>
+                                                            <input type="number" name="kas_pada_bank" value="<?= $this->input->post('kas_pada_bank') ?? $queryeditakt->KAS_PADA_BANK ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Piutang Operasional</label>
+                                                            <input type="number" name="piutang_operasional" value="<?= $this->input->post('piutang_operasional') ?? $queryeditakt->PIUTANG_OPERASIONAL ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Piutang Daya Makara</label>
+                                                            <input type="number" name="piutang_daya_makara" value="<?= $this->input->post('piutang_daya_makara') ?? $queryeditakt->PIUTANG_DAYA_MAKARA ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Piutang Proyek</label>
+                                                            <input type="number" name="piutang_proyek" value="<?= $this->input->post('piutang_proyek') ?? $queryeditakt->PIUTANG_PROYEK ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Piutang TVUI</label>
+                                                            <input type="number" name="piutang_tvui" value="<?= $this->input->post('piutang_tvui') ?? $queryeditakt->PIUTANG_TVUI ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Piutang Solar Car</label>
+                                                            <input type="number" name="piutang_solar_car" value="<?= $this->input->post('piutang_solar_car') ?? $queryeditakt->PIUTANG_SOLAR_CAR ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Komputer</label>
+                                                            <input type="number" name="komputer" value="<?= $this->input->post('komputer') ?? $queryeditakt->KOMPUTER ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Akum.Peny.-Komputer</label>
+                                                            <input type="number" name="akum_peny_komputer" value="<?= $this->input->post('akum_peny_komputer') ?? $queryeditakt->AKUM_PENY_KOMPUTER ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Fax</label>
+                                                            <input type="number" name="fax" value="<?= $this->input->post('fax') ?? $queryeditakt->FAX ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Akum.Peny.-Fax</label>
+                                                            <input type="number" name="akum_peny_fax" value="<?= $this->input->post('akum_peny_fax') ?? $queryeditakt->AKUM_PENY_FAX ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>AC</label>
+                                                            <input type="number" name="ac" value="<?= $this->input->post('ac') ?? $queryeditakt->AC ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Akum.Peny.-AC</label>
+                                                            <input type="number" name="akum_peny_ac" value="<?= $this->input->post('akum_peny_ac') ?? $queryeditakt->AKUM_PENY_AC ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Furniture</label>
+                                                            <input type="number" name="furniture" value="<?= $this->input->post('furniture') ?? $queryeditakt->FURNITURE ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Akum.Peny.-Furniture</label>
+                                                            <input type="number" name="akum_peny_furniture" value="<?= $this->input->post('akum_peny_furniture') ?? $queryeditakt->AKUM_PENY_FURNITURE ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Notebook</label>
+                                                            <input type="number" name="notebook" value="<?= $this->input->post('notebook') ?? $queryeditakt->NOTEBOOK ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Akum.Peny.-Notebook</label>
+                                                            <input type="number" name="akum_peny_notebook" value="<?= $this->input->post('akum_peny_notebook') ?? $queryeditakt->AKUM_PENY_NOTEBOOK ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Kamera Digital</label>
+                                                            <input type="number" name="kamera_digital" value="<?= $this->input->post('kamera_digital') ?? $queryeditakt->KAMERA_DIGITAL ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Akum.Peny.-Kamera Digital</label>
+                                                            <input type="number" name="akum_peny_kamera" value="<?= $this->input->post('akum_peny_kamera') ?? $queryeditakt->AKUM_PENY_KAMERA ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Printer</label>
+                                                            <input type="number" name="printer" value="<?= $this->input->post('printer') ?? $queryeditakt->PRINTER ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Akum.Peny.-Printer</label>
+                                                            <input type="number" name="akum_peny_printer" value="<?= $this->input->post('akum_peny_printer') ?? $queryeditakt->AKUM_PENY_PRINTER ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Kamera Video</label>
+                                                            <input type="number" name="video_kamera" value="<?= $this->input->post('video_kamera') ?? $queryeditakt->VIDEO_KAMERA ?>" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-5">
+                                                            <label>Akum.Peny.-Kamera Video</label>
+                                                            <input type="number" name="akum_peny_video" value="<?= $this->input->post('akum_peny_video') ?? $queryeditakt->AKUM_PENY_VIDEO ?>" class="form-control">
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col-md-4">
+                                                                <div class="form-group col-6">
+                                                                    <button type="submit" class="btn btn-success">Simpan</button>
+                                                                </div>
+                                                            </div>
+                                                            <!-- >>>>>>> Stashed changes -->
                                                         </div>
                                                     </div>
                                                 </div>
-                                        </div>
-
-                                    <?php } ?>
-                                    </div>
-                                    <form action="<?php echo base_url() . 'transaksi/update'; ?>" method="post">
-                                        <div class="col-md-0 mt-4">
-                                            <div class="col-md-2">
-                                                <ul class="list-group">
-                                                    <div class="card-body px-3 pt-1 pb-0">
-                                                        <center>
-                                                            <div class="row">
-                                                                <div class="col-md-4">
-                                                                    <div class="form-group col-6">
-                                                                        <button type="submit" href="#" class="btn btn-success">Simpan</button>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="col-md-6">
-                                                                    <div class="form-group col-4">
-                                                                        <button type="reset" class="btn btn-danger">Batal</button>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </center>
-                                                    </div>
-                                                </ul>
                                             </div>
-                                        </div>
+                                            <!-- <<<<<<< Updated upstream <?php } ?> </div>
+                                            <form action="<?php echo base_url() . 'transaksi/update'; ?>" method="post">
+                                                =======
+                                            </form>
+
+                                            <form action="" method="post"> -->
+                                            <!-- >>>>>>> Stashed changes -->
+                                            <!-- <div class="col-md-0 mt-4">
+                                                    <div class="col-md-2">
+                                                        <ul class="list-group">
+                                                            <div class="card-body px-3 pt-1 pb-0">
+                                                                <center>
+                                                                    <div class="row">
+                                                                        <div class="col-md-4">
+                                                                            <div class="form-group col-6">
+                                                                                <button type="submit" class="btn btn-success">Simpan</button>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </center>
+                                                            </div>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </form> -->
                                     </form>
-                                </form>
                             </div>
                         </div>
-
-
-
                         <footer class="footer pt-3  ">
                             <div class="container-fluid">
                                 <div class="row align-items-center justify-content-lg-between">
